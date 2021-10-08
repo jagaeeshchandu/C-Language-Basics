19e57ac89077ec905e7330f9e5136a0f0f838d60
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-08 14:08:18 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+scanf("%d%d",&a,&b);
+a+b && a<b && a-b ;
+printf("%d",c); 
+}
---
+++ reading a character.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	char a='Z';
+	printf("enter the character \n");
+	scanf("%c",&a);
+	printf("%d",a);
+}

0a525acb1f1192efa25aebd030babaf12751617e
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-09 15:28:48 +0530

    Add files via upload

---
+++ if else stmt number is even or odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	char a;
+	printf("enter the character");
+	scanf("%c",&a);
+	if(a=='a' || a=='e' || a=='i' ||a=='o' || a=='u' )
+	printf("number is ovel");
+	else
+	printf("number is consonent");	
+}
+

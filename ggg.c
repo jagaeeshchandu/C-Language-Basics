eb5f499a8b03ea776986fe8fae09f9f6ff22215d
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-09 15:28:17 +0530

    Add files via upload

---
+++ ggg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	char a;
+	printf("enter the character");
+	scanf("%c",&a);
+	if(a=='a' || a=='e' || a=='i' ||a=='o' || a=='u' )
+	printf("number is ovel");
+	else
+	printf("number is consonent");	
+}

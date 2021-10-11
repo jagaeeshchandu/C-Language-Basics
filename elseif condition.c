2a0c819367e8953b53d84b319643818424b10058
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-11 14:21:39 +0530

    Add files via upload

---
+++ elseif condition.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter the day");
+	scanf("%d",&a);
+	if(a==1) 
+		printf("sunday");
+		else if(a==2)
+		printf("monday");
+	 else if(a==3)
+	printf("tuesday");
+	else if(a==4)
+		printf("wednesday");
+		else if(a==5)
+		printf("thursday");
+	else if(a==6)
+	printf("friday");
+		else if(a==7)
+		printf("saturday");
+	
+}

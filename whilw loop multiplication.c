1f5b78150e4346b7005ea55b472f192bd682c252
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-20 15:07:37 +0530

    Add files via upload

---
+++ whilw loop multiplication.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int i,n,s;
+	printf("enter the intiger");
+	scanf("%d",&n);
+	printf("enter the range required");
+	scanf("%d",&s);
+	while(i<=n)
+	{
+	printf("%d*%d=%d\n",n,i,n*i);
+	i++;
+	}
+
+	
+}
+
+	

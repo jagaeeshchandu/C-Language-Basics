dd4d8c4c173b38de2afdd4b127b364ff78b015a4
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-10-23 14:31:39 +0530

    Add files via upload

---
+++ number divisible by 20 to 50.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int i,n,sum=0;
+	printf("enter the number divisible by three");
+	scanf("%d",&n);
+	for(i=20;i<=n;i++)
+{  
+	if(i%7==0)
+ printf("the number disible by three is%d\n",i);
+}
+}
